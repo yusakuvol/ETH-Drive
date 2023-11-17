e0621c66977439f65a53524d027b4cd730a7a4e5
yusakuvol <yoursakuyu@gmail.com>
2023-11-17 21:40:15 +0300

    add custom tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        base: "#F7F7F7",
+        "base-blue": "#3888FF",
+        "base-black": "#262626",
+        "base-gray": "#9B9BA6",
+      },
+      fontFamily: {
+        custom: [
+          "Satoshi",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          '"Fira Sans"',
+          '"Droid Sans"',
+          '"Helvetica Neue"',
+          "sans-serif",
+        ],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
